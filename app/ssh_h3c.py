80c29aaaf2bfba286f46988349eda78603caf3b9
philatelist671 <Philatelist671@yandex.ru>
2026-07-16 13:06:16 +0300

    Update ssh_h3c.py

--- app/ssh_h3c.py
+++ app/ssh_h3c.py
@@ -5,9 +5,18 @@ def ssh_exec(host, port, username, password, command, timeout=30):
     transport = paramiko.Transport((host, port))
     transport.sock.settimeout(timeout)
     try:
-        # Разрешаем старые алгоритмы ключей хоста
+        # Регистрируем поддержку устаревших алгоритмов ключей хоста
+        transport._key_info = {
+            'ssh-rsa': paramiko.RSAKey,
+            'ssh-dss': paramiko.DSSKey,
+            'ecdsa-sha2-nistp256': paramiko.ECDSAKey,
+            'ecdsa-sha2-nistp384': paramiko.ECDSAKey,
+            'ecdsa-sha2-nistp521': paramiko.ECDSAKey,
+            'ssh-ed25519': paramiko.Ed25519Key,
+        }
+        # Указываем предпочитаемые алгоритмы (порядок важен)
         transport._preferred_keys = ['ssh-rsa', 'ssh-dss']
-        # Подключаемся с паролем
+        # Подключаемся
         transport.connect(username=username, password=password)
         channel = transport.open_session()
         channel.exec_command(command)
